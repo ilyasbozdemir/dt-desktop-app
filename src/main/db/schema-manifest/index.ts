fed6a568dc77e4ad9b570a9ffa8ebffad4e1c100
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2026-05-31 10:55:14 +0300

    chore: release v1.0.0-alpha.5

--- src/main/db/schema-manifest/index.ts
+++ src/main/db/schema-manifest/index.ts
@@ -3,8 +3,9 @@ import v1alpha1 from './1.0.0-alpha.1.yaml?raw'
 import v1alpha2 from './1.0.0-alpha.2.yaml?raw'
 import v1alpha3 from './1.0.0-alpha.3.yaml?raw'
 import v1alpha4 from './1.0.0-alpha.4.yaml?raw'
+import v1alpha5 from './1.0.0-alpha.5.yaml?raw'
 
 // Sıralama schema_version'a göre ascending (küçükten büyüğe) olmalıdır. Sıra şart!
-export const manifestsRaw = [v1alpha1, v1alpha2, v1alpha3, v1alpha4]
+export const manifestsRaw = [v1alpha1, v1alpha2, v1alpha3, v1alpha4, v1alpha5]
 
 export const manifests = manifestsRaw.map(raw => yaml.load(raw) as any)
